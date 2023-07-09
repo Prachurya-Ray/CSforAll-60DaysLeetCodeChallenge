5428c857fb0a031c44e4c3b58ed591bc14b4064c
Prachurya Ray <70370699+Prachurya-Ray@users.noreply.github.com>
2023-07-09 13:47:15 +0530

    Create Binary Search.cpp

---
+++ Day 3/Binary Search.cpp
@@ -0,0 +1,23 @@
+'''
+  Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+  If target exists, then return its index. Otherwise, return -1.
+'''
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+        while(start<=end)
+        {
+            int mid=((unsigned)start + (unsigned)end)>>1;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        return -1;
+    }
+};
